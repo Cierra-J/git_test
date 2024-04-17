15db3332ba2a70f52aa5fa7cdd6057743b0e2e07
Cierra-J <128351361+Cierra-J@users.noreply.github.com>
2024-04-16 23:53:17 -0500

    Update pythonn.py
    added more practice lines

--- pythonn.py
+++ pythonn.py
@@ -65,4 +65,32 @@ else:
 word = "apothecary"
 for letter in word:  # letter is the loop variable
     print(letter)
-    
+
+
+user_int = int(input("How many integers would you like to enter?: "))
+total_num_letters = 0
+print("Please enter the names of your three favorite animals.")
+for val in range(1,4):
+    animal = input()
+    for letter in animal:
+        total_num_letters += 1
+    print("Those names contained a total of", total_num_letters, "characters")
+
+totalint = int(input("Please enter an integer:"))
+total = 0
+for num in range(1, totalint+1):
+    total += num
+print(total)
+total_ch = 0
+user_str = (input("Enter a sentence: "))
+for ch in user_str:
+    total_ch += 1
+if total_ch % 2 ==0:
+    print("even")
+else:
+        print("odd")
+while True:
+    user_input = input('enter a quote. type quit to exit: ')
+    if "quit" in user_input:
+        break
+    print(user_input)
